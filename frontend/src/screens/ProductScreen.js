0ae7db18ccd733c2167e3e4c031b32bf21389dea
Alek <alek.jakimovski1@gmail.com>
2020-12-26 20:23:10 +0100

    Product Details Screen component

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap'
+import { Row, Col, Image, ListGroup, Card, Button, Container } from 'react-bootstrap'
 import Rating from '../components/Rating';
 import products from '../products'
 
@@ -16,11 +16,45 @@ const ProductScreen = ({ match }) => {
                     <Image src={product.image} alt={product.name} fluid />
                 </Col>
                 <Col md={3}>
-                    <ListGroup variant="flush">
+                    <ListGroup variant="flush" style={{ width: '280px', maxWidth: '500px' }}>
                         <ListGroup.Item>
                             <h3>{product.name}</h3>
                         </ListGroup.Item>
+                        <ListGroup.Item>
+                            <Rating rating={product.rating} text={`${product.numReviews} reviews`} />
+                        </ListGroup.Item>
+                        <ListGroup.Item>Price: ${product.price}</ListGroup.Item>
+                        <ListGroup.Item style={{ fontSize: '1rem' }}>Description: {product.description}</ListGroup.Item>
                     </ListGroup>
+                </Col>
+                <Col md={3} className="my-3" style={{ width: '500px', }}>
+                    <Card>
+                        <ListGroup variant="flush">
+                            <ListGroup.Item>
+                                <Row>
+                                    <Col>
+                                        Price:
+                                    </Col>
+                                    <Col>
+                                        <strong>${product.price}</strong>
+                                    </Col>
+                                </Row>
+                            </ListGroup.Item>
+                            <ListGroup.Item>
+                                <Row>
+                                    <Col>
+                                        Status:
+                                    </Col>
+                                    <Col>
+                                        {product.countInStock > 0 ? 'In Stock' : 'Out Of Stock'}
+                                    </Col>
+                                </Row>
+                            </ListGroup.Item>
+                            <ListGroup.Item>
+                                <Button className="btn-block" type="button" disabled={product.countInStock === 0}>Add To Cart</Button>
+                            </ListGroup.Item>
+                        </ListGroup>
+                    </Card>
                 </Col>
             </Row>
         </>
