bf2965f6b37654c28c3dd16c667db602f266e46f
Alek <alek.jakimovski1@gmail.com>
2020-12-28 18:13:17 +0100

    product list reducers & actions added, implemented redux state into HomeScreen with useDispatch and useSelector

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -2,31 +2,35 @@ import React from 'react'
 import { useState, useEffect } from 'react'
 import { Row, Col } from 'react-bootstrap'
 import Product from '../components/Product';
+import { useDispatch, useSelector } from 'react-redux'
+import { listProducts } from '../redux/actions/productActions'
 import axios from 'axios'
 
 const HomeScreen = () => {
 
-    const [products, setProducts] = useState([]);
+    const dispatch = useDispatch();
+
+    const productList = useSelector(state => state.productList)
+
+    const { loading, error, products } = productList
 
     useEffect(() => {
-        const fetchProducts = async () => {
-            const { data } = await axios.get('/api/products')
+        dispatch(listProducts())
+    }, [dispatch])
 
-            setProducts(data);
-        }
-        fetchProducts()
-    }, [])
 
     return (
         <>
             <h1 style={{ marginTop: '10px' }}>Latest Products</h1>
-            <Row>
-                {products.map(product => (
-                    <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-                        <Product product={product} />
-                    </Col>
-                ))}
-            </Row>
+            {loading ? <h1>Loading...</h1> : error ? <h3>{error}</h3> :
+                <Row>
+                    {products.map(product => (
+                        <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                            <Product product={product} />
+                        </Col>
+                    ))}
+                </Row>
+            }
         </>
     )
 }
