262edf6b4de0313c01bbd2fb15c8e9f9ec360286
Alek <alek.jakimovski1@gmail.com>
2021-01-13 12:02:47 +0100

    added user reducer & actions

---
+++ frontend/src/redux/actions/userActions.js
@@ -0,0 +1,32 @@
+import { USER_LOGIN_REQUEST, USER_LOGIN_SUCCESS } from "../constants/userConstants"
+import axios from 'axios';
+import { PRODUCT_DETAILS_FAIL } from "../constants/productConstants";
+
+export const login = (email, password) => async (dispatch) => {
+    try {
+        dispatch({
+            type: USER_LOGIN_REQUEST
+        })
+
+        const config = {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
+
+        const { data } = await axios.post('/api/users/login', { email, password }, config)
+
+        dispatch({
+            type: USER_LOGIN_SUCCESS,
+            payload: data
+        })
+
+        localStorage.setItem('userInfo', JSON.stringify(data))
+    }
+    catch (error) {
+        dispatch({
+            type: PRODUCT_DETAILS_FAIL,
+            payload: error.response && error.response.data.message ? error.response.data.message : error.message
+        })
+    }
+} 
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -3,17 +3,22 @@ import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import { productListReducer, productDetailsReducer } from './redux/reducers/productReducers'
 import { cartReducer } from './redux/reducers/cartReducers'
+import { userLoginReducer } from './redux/reducers/userReducers'
 
 const reducer = combineReducers({
     productList: productListReducer,
     productDetails: productDetailsReducer,
-    cart: cartReducer
+    cart: cartReducer,
+    userLogin: userLoginReducer
 })
 
 const storageCartItems = localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : []
 
+const userInfoFromStorage = localStorage.getItem('userInfo') ? JSON.parse(localStorage.getItem('userInfo')) : null
+
 const initialState = {
-    cart: { cartItems: storageCartItems }
+    cart: { cartItems: storageCartItems },
+    userLogin: { userInfo: userInfoFromStorage }
 }
 
 const middleware = [thunk]
